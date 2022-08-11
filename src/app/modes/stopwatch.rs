24756958ee3b6b9000ff5fbeeefe18c35d57f99a
Wesley Moore <wes@wezm.net>
2022-08-11 12:09:53 +1000

    Add Pause trait for stopwatch and timer

--- src/app/modes/stopwatch.rs
+++ src/app/modes/stopwatch.rs
@@ -2,6 +2,8 @@ use chrono::{DateTime, Duration, Local};
 use clock_tui::bricks_text::BricksText;
 use tui::{buffer::Buffer, layout::Rect, style::Style, widgets::Widget};
 
+use crate::app::Pause;
+
 use super::{format_duration, render_centered, DurationFormat};
 
 pub struct Stopwatch {
@@ -18,24 +20,6 @@ impl Stopwatch {
             style,
             duration: Duration::zero(),
             started_at: Some(Local::now()),
-        }
-    }
-
-    pub(crate) fn is_paused(&self) -> bool {
-        self.started_at.is_none()
-    }
-
-    pub(crate) fn pause(&mut self) {
-        if let Some(start_at) = self.started_at {
-            let now = Local::now();
-            self.duration = self.duration + now.signed_duration_since(start_at);
-            self.started_at = None;
-        }
-    }
-
-    pub(crate) fn resume(&mut self) {
-        if self.started_at.is_none() {
-            self.started_at = Some(Local::now());
         }
     }
 
@@ -59,5 +43,25 @@ impl Widget for &Stopwatch {
             None
         };
         render_centered(area, buf, &text, None, footer);
+    }
+}
+
+impl Pause for Stopwatch {
+    fn is_paused(&self) -> bool {
+        self.started_at.is_none()
+    }
+
+    fn pause(&mut self) {
+        if let Some(start_at) = self.started_at {
+            let now = Local::now();
+            self.duration = self.duration + now.signed_duration_since(start_at);
+            self.started_at = None;
+        }
+    }
+
+    fn resume(&mut self) {
+        if self.started_at.is_none() {
+            self.started_at = Some(Local::now());
+        }
     }
 }
